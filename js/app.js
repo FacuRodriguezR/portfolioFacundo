e48fe7e9346c65d73c0a0a16882b27cec9452384
Facundo Rodriguez <facurtprn@gmail.com>
2022-09-14 12:21:43 -0300

    profile section, profile skills, profile contact

--- js/app.js
+++ js/app.js
@@ -8,3 +8,32 @@ function toggleMenu(){
 
 openMenu.addEventListener("click", toggleMenu);
 closeMenu.addEventListener("click", toggleMenu);
+
+
+const menuLinks = document.querySelectorAll(" .menu a[href^='#']");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute("id");
+        const menuLink = document.querySelector(` .menu a[href^="#${id}"]`);
+
+        if (entry.isIntersecting){
+            menuLink.classList.add("selected");
+        } else {
+            menuLink.classList.remove("selected")
+        }
+    })
+})
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened")
+    })
+
+    const hash = menuLinks.getAttribute("href");
+    const target = document.querySelector(hash);
+
+    if  (target) {
+        observer.observe(target);
+    }
+})
